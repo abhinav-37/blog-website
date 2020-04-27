f534d1f9bec8326009625ea38a22ede5714dc4b1
abhinav sharma <abhiut37@gmail.com>
2020-04-27 23:17:36 +0530

    changed port and gitignore

--- app.js
+++ app.js
@@ -198,6 +198,8 @@ app.post("/showSingle",function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 8000;
+  }
+  app.listen(port);
